09757d69809b291869c7f24c8ac2d8526046e93f
Yahaira Tituaña <yatg2495@gmail.com>
2022-05-18 00:38:29 -0500

    Practicando Java

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+    //Declarando una variable
+        int speed;
+        speed=50;
+        System.out.println(speed);
+        int salary = 1000;
+        String employeeName= "Yahis Tituaña";
+        System.out.println(employeeName);
+    }
+}
